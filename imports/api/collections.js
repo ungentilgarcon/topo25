9d75a9bebc2c14f813e832bfe6d8dcce7fd3d759
ungentilgarcon <monsieur.goonie@gmail.com>
2025-10-03 12:52:01 +0200

    fix(eslint): define Users in collections.js, enable JSX parsing via @babel/plugin-syntax-jsx; temporarily re-ignore imports/** for incremental lint

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -1,10 +1,14 @@
 import { Mongo } from 'meteor/mongo'
+import { Meteor } from 'meteor/meteor'
 
 import { Topograms } from '/imports/api/topograms/Topograms.js'
 import { Nodes } from '/imports/api/nodes/Nodes.js'
 import { Edges } from '/imports/api/edges/Edges.js'
 
 export const Comments = new Mongo.Collection( 'comments' )
+
+// Expose Users alias so legacy imports can reference it; Meteor maintains this collection.
+export const Users = Meteor.users
 
 export {
   Nodes,
